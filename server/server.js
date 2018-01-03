e2b4c489e7ac0993fca928bbac2cde9b49ec74cc
Simon Stubben <simonstubben@gmail.com>
2018-01-03 20:47:14 +0100

    Added GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -2,6 +2,8 @@ var express = require('express');
 var bodyParser = require('body-parser');
 
 var{mongoose} = require('./db/mongoose')
+const {ObjectID} = require('mongodb')
+
 var {Todo} = require('./models/todo')
 var {User} = require('./models/user')
 
@@ -26,6 +28,24 @@ app.get('/todos', (req, res) => {
     res.send({todos})
   }, (e) => {
     res.status(400).send(e)
+  })
+})
+
+app.get('/todos/:id', (req, res) => {
+  // req.params will include the key/value pair
+  // where the key is the url parameter, like id, and
+  // the value is the value of the key
+  var id = req.params.id
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send()
+  }
+  Todo.findById(id).then((todo) => {
+    if (!todo) {
+      return res.status(400).send()
+    }
+    res.send(todo)
+  }, (e) => {
+    res.status(400).send()
   })
 })
 
