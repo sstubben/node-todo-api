cab8a9d7ba4221578013b82fc8ce370549d9d9bb
Simon Stubben <simonstubben@gmail.com>
2018-04-26 21:31:04 +0200

    Added test to DELETE /users/me/token. Updated packages

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -291,8 +291,27 @@ describe("POST /users/login", () => {
         }
         User.findById(users[1]._id).then((user) => {
           expect(user.tokens.length).toBe(0)
-        })
-        done()
-      }).catch((e) => done(e))
-  });
-});
+          done()
+        }).catch((e) => done(e))
+      })
+  })
+})
+
+describe("DELETE /users/me/token", () => {
+  // when a real valid x-auth token gets passed across
+  it("should remove auth token on logout", (done) => {
+    request(app)
+      .delete('/users/me/token')
+      .set('x-auth', users[0].tokens[0].token)
+      .expect(200)
+      .end((err, res) => {
+        if (err) {
+          return done(err)
+        }
+        User.findById(users[0]._id).then((user) => {
+          expect(user.tokens.length).toBe(0)
+          done()
+        }).catch((e) => done(e))
+      })
+  })
+})
